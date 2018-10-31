25c6cad9876347d5ec5bf703289b0443e3224770
Ridho Pratama <p.ridho@yahoo.co.id>
2018-10-31 20:18:31 +0700

    Added argv command

--- server/server.cpp
+++ server/server.cpp
@@ -1,78 +1,92 @@
-#include <stdio.h> 
-#include <sys/socket.h> 
+#include <stdio.h>
+#include <sys/socket.h>
 #include <iostream>
 #include <unistd.h>
 
 #include "server.hpp"
 #include "../packet/ack.hpp"
 #include "../packet/frame.hpp"
-#include "../file/file_writer.hpp";
+#include "../file/file_writer.hpp"
 
+namespace server
+{
+using namespace packet;
+using namespace sw;
+using namespace file;
 
-namespace server {
-	using namespace packet;
-	using namespace sw;
-	using namespace file;
+Server::Server(int port, char *destinationFile)
+{
+	this->destinationFile = destinationFile;
 
-	Server::Server(int port) {
-		this->port = port;
-		this->address.sin_family = AF_INET; 
-	    this->address.sin_addr.s_addr = INADDR_ANY; 
-	    this->address.sin_port = htons(port);
-	    this->window = SlidingWindow(10);
-	    this->sock = socket(AF_INET, SOCK_DGRAM, 0);
-	    bind((this->sock), (struct sockaddr *)&(this->address), sizeof(this->address));
-	}
+	this->port = port;
+	this->address.sin_family = AF_INET;
+	this->address.sin_addr.s_addr = INADDR_ANY;
+	this->address.sin_port = htons(port);
+	this->window = SlidingWindow(10);
+	this->sock = socket(AF_INET, SOCK_DGRAM, 0);
+	bind((this->sock), (struct sockaddr *)&(this->address), sizeof(this->address));
+}
 
-	Server::~Server() {
-		close(this->sock);
-	}
+Server::~Server()
+{
+	close(this->sock);
+}
 
-	void Server::listenForClients() {
-		sockaddr clientAddress;
-		unsigned int addrlen;
-		char * buffer;
-		char * data;
-		int dataLength;
-		bool end = false;
-		while (!end) {
-			buffer = new char[1034];
-			recvfrom((this->sock) , buffer, 1034, 0, &(clientAddress), &addrlen);
-			Frame frame(buffer);
-			printf("RECEIVED FRAME : %d\n", frame.getSeqNum());
-			this->processFrame(frame, clientAddress);
-			delete [] buffer;
-			end = frame.getSOH() == 0x4;
-			if (end) {
-				writeFramesToFile("examples/test-result.flac", this->window.frames);
-			}
+void Server::listenForClients()
+{
+	sockaddr clientAddress;
+	unsigned int addrlen;
+	char *buffer;
+	char *data;
+	int dataLength;
+	bool end = false;
+	while (!end)
+	{
+		buffer = new char[1034];
+		recvfrom((this->sock), buffer, 1034, 0, &(clientAddress), &addrlen);
+		Frame frame(buffer);
+		printf("RECEIVED FRAME : %d\n", frame.getSeqNum());
+		this->processFrame(frame, clientAddress);
+		delete[] buffer;
+		end = frame.getSOH() == 0x4;
+		if (end)
+		{
+			// writeFramesToFile("examples/test-result.flac", this->window.frames);
+			writeFramesToFile(this->destinationFile, this->window.frames);
 		}
-	}
-
-	void Server::processFrame(Frame& frame, sockaddr clientAddress) {
-		if (frame.checkValidity()) {
-			this->window.addFrame(frame);
-			this->replyACK(frame.getSeqNum(), clientAddress);
-		} else {
-			this->replyNACK(frame.getSeqNum(), clientAddress);
-		}
-	}
-
-	void Server::replyACK(int seqNum, sockaddr clientAddress) {
-		ACK ack(0x6, seqNum+1);
-		this->window.addACK(ack);
-		char * buffer = ack.serialize();
-		sendto(this->sock, buffer, 6, 0, &(clientAddress), sizeof(clientAddress));
-		free(buffer);
-		printf("SENT ACK : %d\n", ack.getNextSeqNum() -1);
-	}
-
-	void Server::replyNACK(int seqNum, sockaddr clientAddress) {
-		ACK ack(0x2, seqNum+1);
-		printf("SENT NACK : %d\n", ack.getNextSeqNum() -1);
-		this->window.addACK(ack);
-		char * buffer = ack.serialize();
-		sendto(this->sock, ack.serialize(), 6, 0, &(clientAddress), sizeof(clientAddress));
-		free(buffer);
 	}
 }
+
+void Server::processFrame(Frame &frame, sockaddr clientAddress)
+{
+	if (frame.checkValidity())
+	{
+		this->window.addFrame(frame);
+		this->replyACK(frame.getSeqNum(), clientAddress);
+	}
+	else
+	{
+		this->replyNACK(frame.getSeqNum(), clientAddress);
+	}
+}
+
+void Server::replyACK(int seqNum, sockaddr clientAddress)
+{
+	ACK ack(0x6, seqNum + 1);
+	this->window.addACK(ack);
+	char *buffer = ack.serialize();
+	sendto(this->sock, buffer, 6, 0, &(clientAddress), sizeof(clientAddress));
+	free(buffer);
+	printf("SENT ACK : %d\n", ack.getNextSeqNum() - 1);
+}
+
+void Server::replyNACK(int seqNum, sockaddr clientAddress)
+{
+	ACK ack(0x2, seqNum + 1);
+	printf("SENT NACK : %d\n", ack.getNextSeqNum() - 1);
+	this->window.addACK(ack);
+	char *buffer = ack.serialize();
+	sendto(this->sock, ack.serialize(), 6, 0, &(clientAddress), sizeof(clientAddress));
+	free(buffer);
+}
+} // namespace server
--- server/server.hpp
+++ server/server.hpp
@@ -5,24 +5,28 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-namespace server {
-	using namespace packet;
-	using namespace sw;
+namespace server
+{
+using namespace packet;
+using namespace sw;
 
-	class Server {
-	private:
-		struct sockaddr_in address;
-		int port;
-		int sock;
-		SlidingWindow window;
-	public:
-		Server(int port);
-		~Server();
-		void listenForClients();
-		void processFrame(Frame& frame, sockaddr clientAddress);
-		void replyACK(int seqNum, sockaddr clientAddress);
-		void replyNACK(int seqNum, sockaddr clientAddress);
-	};
-}
+class Server
+{
+  private:
+	struct sockaddr_in address;
+	int port;
+	int sock;
+	SlidingWindow window;
+	char *destinationFile;
+
+  public:
+	Server(int port, char *destinationFile);
+	~Server();
+	void listenForClients();
+	void processFrame(Frame &frame, sockaddr clientAddress);
+	void replyACK(int seqNum, sockaddr clientAddress);
+	void replyNACK(int seqNum, sockaddr clientAddress);
+};
+} // namespace server
 
 #endif
